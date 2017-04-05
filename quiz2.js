89af6080df922e26062226bc29d0b7cd071bff9d
kwilliston3 <knw19@pitt.edu>
2017-04-04 21:14:23 -0400

    Add files via upload

--- quiz2.js
+++ quiz2.js
@@ -1,3 +1,32 @@
-consle.log('welocme to quiz town, population...YOU!')
 
-@('.dynamic').hmtl('<div p>HI THERE, I\'M FROM JAVASCRIPT</p></div>').fadeIn('slo')
+var firstname = $("#first_name").value;
+var lastname  = $("#last_name").value;
+var emailname = $("#email_name").value;
+var valid = true;
+$('#join_list').click(function(e) {
+  e.preventDefault();
+  if ($("#first_name").val() === '') {
+    $('#first_name_error').text('First name is required').css('color', 'red');
+    valid = false;
+  } else {
+    $('#first_name_error').text('');
+    valid = true;
+  }
+  if ($("#last_name").val() === '') {
+    $('#last_name_error').text('Last name is required').css('color', 'red');
+    valid = false;
+  } else {
+    $('#last_name_error').text('');
+    valid = true;
+  }
+  if ($("#email_name").val() === '') {
+    $('#email_name_error').text('Email is required').css('color', 'red');
+    valid = false;
+  } else {
+    $('#email_name_error').text('');
+    valid = true;
+  }
+  if (valid) {
+    $('#email_form').submit();
+  }
+});
